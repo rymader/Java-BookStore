3c1926eb94443dd641e7b56528d5fea86d6d3d61
Ryan Mader <rymader@yahoo.com>
2018-07-25 18:59:07 -0500

    module 3

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -67,6 +67,9 @@ public class ControllerServlet extends HttpServlet {
                 case "/edit":
                     showEditForm(request, response);
                     break;
+                case "/update":
+                    updateBook(request, response);
+                    break;
                 default:
                     listBooks(request, response);
                     break;
@@ -129,6 +132,17 @@ public class ControllerServlet extends HttpServlet {
         dispatcher.forward(request, response);
     }
 
+    private void updateBook(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String title = request.getParameter("booktitle");
+        String author = request.getParameter("bookauthor");
+        Float price = Float.parseFloat(request.getParameter("bookprice"));
+        Book book = new Book(id, title, author, price);
+        bookDAO.updateBook(book);
+        response.sendRedirect("list");
+    }
+
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
@@ -137,7 +151,6 @@ public class ControllerServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("This is the doPost() method!");
         doGet(request, response);
-
     }
 
 }
